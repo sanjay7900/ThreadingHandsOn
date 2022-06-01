9b4e96bc58f5f30dc0873bc993fd7b8bc19b1352
sanjay7900 <singhsanjay790043@gmail.com>
2022-06-01 15:37:03 +0530

    initial commit

---
+++ ThreadingInCSharp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+
+namespace ThreadingInCSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ThreadingClass threadingClass = new ThreadingClass();
+
+            Thread Thread1 = new Thread(new ThreadStart(threadingClass.Thread1));
+            Thread Thread2 = new Thread(new ThreadStart(threadingClass.Thread2));
+            Thread1.Start();
+            Thread2.Start();
+            Console.WriteLine("-------------------------------------");
+            Thread Thread3 = new Thread(new ThreadStart(threadingClass.TreadingWithForLoop1));
+            Thread Thread4 = new Thread(new ThreadStart(threadingClass.TreadingWithForLoop2));
+            Thread3.Start();
+            Thread4.Start();
+            Console.WriteLine("-----------------------------------");
+            Thread Thread5 = new Thread(new ThreadStart(threadingClass.LockWithTread1));
+            Thread Thread6 = new Thread(new ThreadStart(threadingClass.LockWithTread2));
+            Thread5.Start();
+            Thread6.Start();
+            Console.ReadKey();  
+
+        }
+    }
+}
---
+++ ThreadingInCSharp/ThreadingClass.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ThreadingInCSharp
+{
+    public class ThreadingClass
+    {
+       private string data;
+        public void Thread1()
+        {
+            Console.WriteLine("i m Thread 1");
+
+        }
+        public void Thread2()
+        {
+            Console.WriteLine("i m Thread 2");
+
+        }
+       
+        public void TreadingWithForLoop1()
+        {
+            for(int i = 0; i <= 100; i+=2)
+            {
+                Console.WriteLine("i m Even: "+i);
+            }
+            Thread.Sleep(1000);
+
+        }
+        public void TreadingWithForLoop2()
+        {
+            for (int i = 1; i <= 100; i+=3)
+            {
+                Console.WriteLine("i m odd: "+i);
+            }
+            Thread.Sleep(5000);
+
+        }
+
+
+        public void LockWithTread1()
+        {
+            while(true)
+            {
+                lock (this)
+                {
+                    Console.WriteLine("Display Thread 1");
+                    data = "Hello Thread1";
+                    Thread.Sleep(1000);    
+                    Console.WriteLine("Thread 1 Output --> {0}", data);
+                }
+            }
+
+        }
+        public void LockWithTread2()
+        {
+            while (true)
+            {
+                lock (this)
+                {
+                    Console.WriteLine("Display Thread 2");
+                    data = "Hello Thread2";
+                    Thread.Sleep(1000);
+                    Console.WriteLine("Thread 1 Output --> {0}" , data);
+                }
+            }
+
+        }
+
+    }
+}
